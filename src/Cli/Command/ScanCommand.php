cf8c33d00fd8aa0ca94998e223587d17f2405497
Nils Langner <contact@phmlabs.com>
2016-01-07 14:07:01 +0100

    added ignore list

--- src/Cli/Command/ScanCommand.php
+++ src/Cli/Command/ScanCommand.php
@@ -14,7 +14,6 @@ use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Yaml\Yaml;
 use whm\JsErrorScanner\PhantomJS\ErrorRetriever;
 use whm\JsErrorScanner\PhantomJS\HarRetriever;
-use whm\JsErrorScanner\PhantomJS\PhantomJsRuntimeException;
 use whm\JsErrorScanner\Reporter\Incident;
 use whm\JsErrorScanner\Reporter\XUnit;
 
@@ -27,6 +26,7 @@ class ScanCommand extends Command
                 new InputArgument('url', InputArgument::REQUIRED, 'url to be checked'),
                 new InputOption('koalamon_project', 'p', InputOption::VALUE_OPTIONAL, 'the koalamon project', null),
                 new InputOption('koalamon_project_api_key', 'k', InputOption::VALUE_OPTIONAL, 'the koalamon api key', null),
+                new InputOption('ignore_list', 'i', InputOption::VALUE_OPTIONAL, 'the irgnoe list file', null),
             ))
             ->setDescription('Check an url for js errors.')
             ->setName('scan');
@@ -43,17 +43,28 @@ class ScanCommand extends Command
         $errorRetriever = new ErrorRetriever();
         $errors = $errorRetriever->getErrors(new Uri($input->getArgument('url')));
 
+        $ignores = array();
+        $errorFound = false;
+
+        if ($input->getOption('ignore_list')) {
+            $ignoreFile = Yaml::parse(file_get_contents($input->getOption('ignore_list')));
+            $ignores = $ignoreFile['ignore'];
+        }
+
         if (count($errors) > 0) {
             $errorMsg = 'JavaScript errors (' . count($errors) . ') where found  on ' . $input->getArgument('url') . '<ul>';
             foreach ($errors as $error) {
-                $output->writeln('  - ' . $error);
-                $errorMsg .= '<li>' . $error . '</li>';
+                if (!in_array($error, $ignores)) {
+                    $output->writeln('  - ' . $error);
+                    $errorMsg .= '<li>' . $error . '</li>';
+                    $errorFound = true;
+                }
             }
-
             $errorMsg .= '</ul>';
             $status = Event::STATUS_FAILURE;
+        }
 
-        } else {
+        if (!$errorFound) {
             $output->writeln('   No errors found.');
             $errorMsg = '';
             $status = Event::STATUS_SUCCESS;
@@ -63,7 +74,7 @@ class ScanCommand extends Command
             $reporter = new Reporter($input->getOption('koalamon_project'), $input->getOption('koalamon_project_api_key'), new Client());
             $system = str_replace('http://', '', $input->getArgument('url'));
             $event = new Event('JsErrorScanner_' . $system, $system, $status, 'JsErrorScanner', $errorMsg, count($errors));
-            $reporter->sendEvent($event);
+            $reporter->sendEvent($event, true);
         }
 
         $output->writeln('');
