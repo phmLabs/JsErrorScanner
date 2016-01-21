95555f1cf11134394e5f7aad7a57a39f3c9afa48
Nils Langner <contact@phmlabs.com>
2016-01-21 13:08:43 +0100

    removed timeout

--- src/PhantomJS/errors.js
+++ src/PhantomJS/errors.js
@@ -7,7 +7,6 @@ if (system.args.length === 1) {
 } else {
 
     page.settings.userAgent = 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/37.0.2062.120 Safari/537.36';
-    page.settings.resourceTimeout = 1000;
 
     page.address = system.args[1];
     page.resources = [];
@@ -19,10 +18,6 @@ if (system.args.length === 1) {
             message = message + '(' + trace[0].file + ':' + trace[0].line + ')';
         }
         console.log(message);
-    };
-
-    page.onResourceTimeout = function (request) {
-        console.log('Response (#' + request.id + '): ' + JSON.stringify(request));
     };
 
     page.open(page.address, function (status) {
