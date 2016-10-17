9417b3664cb7d9dcfbb7ebe760a20d4ddeed56cc
Nils Langner <contact@phmlabs.com>
2016-10-17 14:39:54 +0200

    debug mode for extension

--- src/ErrorRetriever/Webdriver/extension_src/myscript.js
+++ src/ErrorRetriever/Webdriver/extension_src/myscript.js
@@ -4,12 +4,46 @@
 
 localStorage.setItem("js_errors", "");
 
+var jserror_debug = true;
+
+function domToString(element, maxDepth, currentDepth) {
+
+    var result = "";
+
+    if (!currentDepth) {
+        result = "ErrorEvent"
+        currentDepth = 1;
+    }
+
+    if (currentDepth == maxDepth) {
+        return '';
+    }
+
+    for (var key in element) {
+        if (element[key] instanceof Object) {
+            result = result + "\n" + " ".repeat(currentDepth * 2) + key + ': ' + domToString(element[key], maxDepth, currentDepth + 1);
+        } else {
+            result = result + "\n" + " ".repeat(currentDepth * 2) + key + ': ' + element[key];
+        }
+    }
+    return result;
+}
+
 window.addEventListener("error", function (error) {
     message = error.message + ' (filename: ' + error.filename + '::' + error.lineno + ')';
     console.log('Leankoala: ' + message);
     if (localStorage.getItem("js_errors") == "") {
         localStorage.setItem("js_errors", message);
+
+        if (jserror_debug) {
+            errorString = domToString(error, 3);
+            localStorage.setItem("full_errors", errorString);
+        }
     } else {
         localStorage.setItem("js_errors", localStorage.getItem("js_errors") + ' ### ' + message);
+
+        if (jserror_debug) {
+            localStorage.setItem("full_errors", localStorage.getItem("js_errors") + '###' + domToString(error, 3));
+        }
     }
 });
